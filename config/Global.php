383155218951e07e38d6b20e69b58118d7084660
sebastianMeguillanes <sebastian.meguillanes@ucb.edu.bo>
2023-09-12 08:46:44 -0400

    sebas sabe

--- config/Global.php
+++ config/Global.php
@@ -1,22 +1,22 @@
 <?php 
 
 //Ip de la pc servidor de base de datos
-define("DB_HOST","localhost");
+define("DB_HOST","181.188.156.195");
 
 //Nombre de la base de datos
 define("DB_NAME", "doc_final");
 
 //Usuario de la base de datos
-define("DB_USERNAME", "postgres");
+define("DB_USERNAME", "admin");
 
 //Contraseña del usuario de la base de datos
-define("DB_PASSWORD", "Alhacen2002");
+define("DB_PASSWORD", "admin1234");
 
 //definimos la codificación de los caracteres
 define("DB_ENCODE","utf8");
 
 //Usuario de la base de datos
-define("DB_PORT", "5432");
+define("DB_PORT", "18004");
 
 //Definimos una constante como nombre del proyecto
 define("PRO_NOMBRE","Sistema de Ventas para la Universidad Católica Boliviana");
